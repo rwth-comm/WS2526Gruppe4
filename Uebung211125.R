dda9514af96355d9246c76d95eb181eb02f3d832
lbxhix2000 <gina.koetter@rwth-aachen.de>
2025-12-04 11:06:16 +0100

    S

--- Uebung211125.R
+++ Uebung211125.R
@@ -1,10 +1,10 @@
 # Pakete installieren
 
-install.packages("tidyverse")
-install.packages("psych")
-install.packages("plotrix")
-install.packages("remotes")
-remotes::install_github("statisticsforsocialscience/dataforsocialscience")
+#install.packages("tidyverse")
+#install.packages("psych")
+#install.packages("plotrix")
+#install.packages("remotes")
+#remotes::install_github("statisticsforsocialscience/dataforsocialscience")
 
 # Pakete aktivieren
 library(tidyverse)
@@ -14,3 +14,14 @@ library(dataforsocialscience)
 
 # Daten einlesen
 df <- robo_care
+table(df$gender)
+library(ggplot2)
+qplot(df$gender)
+median(df$age)
+quantile(df$age)
+boxplot(df$age)
+
+mean(c(1,2,3,4,5))
+mean(df$technical_knowledge)
+
+psych::describe(df)
