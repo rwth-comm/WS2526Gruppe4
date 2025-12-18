a0659cc4b4fd1964004a5a2fea71de84cc47931a
lbxhix2000 <gina.koetter@rwth-aachen.de>
2025-12-18 17:45:59 +0100

    Data Cleaning

--- AnalyseScript.R
+++ AnalyseScript.R
@@ -1,26 +1,71 @@
- install.packages("tidyverse")
- install.packages("psych")
- 
- library(tidyverse)
+# Pakete aktivieren  ----
+library(tidyverse)
  library(psych)
+ source("qualtricshelpers.R")
  
- raw <-read_csv("data/datacleaning_Beispieldaten.csv")
-raw <- raw[c(-1,-2),] 
+# Daten einlesen ----
+raw <-load_qualtrics_csv("data/datacleaning_Beispieldaten.csv")
   
-raw <- filter(raw, Progress == 100)
+# Rohdaten filtern ----
+raw %>% 
+  filter (Progress == 100) %>% 
+  filter(Status == 4) -> raw
 
+# Überflüssige Variablen entfernen ----
 raw.short <- raw[,c(6,9,18:54)] 
 
-dput(names(raw.short))
+# Variablen umbenennen ----
+generate_codebook(raw.short, "data/datacleaning_Beispieldaten.csv", "data/codebook.csv")
+codebook <- read_codebook("data/codebook_final.csv")
+names(raw.short) <- codebook$variable
 
-names(raw.short) <- c("Duration", "ResponseId", "Age", "Gender", "Edu", "JobType",
-                      "bf_1n", "bf_2", "bf_3n", "bf_4n", "bf_5n",
-                      "bf_6", "bf_7", "bf_8", "bf_9", "bf_10",
-                      "ati_1", "ati_2", "ati_3n", "ati_4", "ati_5",
-                      "ati_6n", "ati_7", "ati_8n", "ati_9",
-                      "wrfq_1", "wrfq_2", "wrfq_3", "wrfq_4", "wrfq_5",
-                      "wrfq_6", "wrfq_7", "wrfq_8", "wrfq_9", 
-                      "svi_1n", "svi_2n", "svi_3", "svi_4n", "svi_5")
+# Korrekte Datentypen zuordnen ----
+raw.short$Age %>% 
+ as.numeric() -> raw.short$Age
 
+raw.short$Gender %>% 
+  recode(`1`= "Männlich",`2`= "Weiblich", `3`= "Divers") %>% 
+  as.factor() -> raw.short$Gender
+  
+  
+  
+raw.short$Edu %>% 
+  ordered(levels = c(1:5),
+            labels = c("Haupt- oder Realschulabschluss",
+                        "Fach-/Hochschulreife (Abitur)",
+                        "Ausbildung",
+                        "Hochschulabschluss",
+                        "Promotion")) -> raw.short$Edu
 
+raw.short$JobType %>%  recode(`1` = "In Ausbildung /Studium",
+                              `2` = "Arbeitnehmer/-in und Studierende/-r",
+                              `3` = "Arbeitnehmer/-in",
+                              `4` = "Arbeitgeber/-in",
+                              `5` = "Selbstständig ohne Mitarbeiter", 
+                              `6` = "Rentner/-in") %>% 
+as.factor() -> raw.short$JobType
+  
+# Qualitätakontrolle ----
 
+# Skalenwerte berechnen ----
+
+schluesselliste <- list (
+  BF_Extraversion = c("-bf_1n", "bf_6"),
+  BF_Agreeableness = c("bf_2", "-bf_7n"),
+  BF_Openness = c("-bf_5n", "bf_10"),
+  BF_Neuroticism = c("-bf_4n", "bf_9"),
+  BF_Concientiousness = c("-bf_3n", "bf_8"),
+  ATI = vars4psych(raw.short, "ati"),
+  PRO = c("wrfq_1", "wrfq_2", "wrfq_3", "wrfq_4","wrfq_9"),
+  PRE = c("wrfq_5", "wrfq_6", "wrfq_7", "wrfq_8"),
+  SVI = vars4psych(raw.short, "svi")
+  )
+
+scores <- scoreItems(schluesselliste, items = raw.short, min = 1, max = 6)
+
+scores$alpha
+
+data <- bind_cols(raw.short, scores$scores)
+
+# Daten exportieren ----
+write_rds(data, "data/data.rds")
